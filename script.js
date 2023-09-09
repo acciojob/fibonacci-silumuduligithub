f374becf3cadb43050aec8715472e469472a6d61
Silu Muduli <130176819+silumuduligithub@users.noreply.github.com>
2023-09-09 12:15:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+function fun(num){
+	if(num == 0 || num == 1)return num;
+	return fun(num - 1) + fun(num - 2);
+}
 function fibonacci(num) {
 // your code here
+	return fun(num);
 }
 
 module.exports = fibonacci;
